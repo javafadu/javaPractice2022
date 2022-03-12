654b34508ada0ede7668fd816eff6a65eabc8a62
Des_Abd_Wis <destiniyabidingwisdom@gmail.com>
2022-03-12 13:29:18 +0300

    12.03.22

--- src/practice_basic_day03/Q12_MetCreationForLoop.java
+++ src/practice_basic_day03/Q12_MetCreationForLoop.java
@@ -8,22 +8,4 @@ public class Q12_MetCreationForLoop {
 		 Write a Java program to reverse a string. Use for loop and create a method called
 		  reverseString */
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("bir string giriniz :");
-        String str = scan.nextLine();
-        System.out.println("string ifadenin tersi : " + reverseString(str));
-
-    }
-
-    public static String reverseString (String str) {
-        String reversed = "";
-        for (int i= str.length()-1;i>=0;i--) {
-            reversed += str.charAt(i);
-        }
-        return reversed;
-
-    }
-
-
 }
--- src/practice_basic_day03/Q16_DoWhileLoop02.java
+++ src/practice_basic_day03/Q16_DoWhileLoop02.java
@@ -12,25 +12,6 @@ public class Q16_DoWhileLoop02 {
 		     OUTPUT 	:   l
 		     INPUT      :   elif
 		     OUTPUT 	:   You entered wrong word.*/
-    public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        String word = "";
-
-        do {
-            System.out.print("bir string giriniz :");
-            word = scan.nextLine();
-            if(word.length()<3) {
-                System.out.println("string 3 karakterden az");
-            }
-            if(word.length()%2==1 && word.length()>=3) {
-                System.out.println("string ortasindaki karakter :" +word.charAt(word.length()/2));
-            }
-            else {
-                System.out.println("You entered wrong word.(string deki karakter sayisi cift)");
-            }
-        }while(!(word.length()%2==1 && word.length()>=3) );
-
-    }
 
 }
