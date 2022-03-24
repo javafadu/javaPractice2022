2bd082764ed90fa01a32df45ab2d9b35973d8740
Des_Abd_Wis <destiniyabidingwisdom@gmail.com>
2022-03-24 20:05:41 +0300

    24.03.22

--- src/practice_free2/Q04.java
+++ src/practice_free2/Q04.java
@@ -1,7 +1,5 @@
 package practice_free2;
 
-import java.util.Scanner;
-
 public class Q04 {
     /*
      * Kullanicidan bir cumle ve bir harf alin,
@@ -14,23 +12,4 @@ public class Q04 {
      *
      * OUTPUT : Girdiginiz cumlede "e" harfi 3 kere kullanilmis.
      */
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("lutfen bir cumle giriniz :");
-        String cumle = scan.nextLine().toLowerCase();
-
-        System.out.print("saydirmak istediginiz harfi giriniz :");
-        char harf = scan.next().toLowerCase().charAt(0);
-
-        int sayac = 0;
-
-        for (int i = 0; i <cumle.length(); i++) {
-            if(cumle.charAt(i)==harf){
-                sayac++;
-            }
-        }
-        System.out.println("Girdiginiz cumlede " + harf +" karakteri " + sayac + " adettir");
-
-
-    }
 }
