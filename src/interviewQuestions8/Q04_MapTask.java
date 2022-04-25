0c1d0841d177255152b9963e724a763cb02463d4
Des_Abd_Wis <destiniyabidingwisdom@gmail.com>
2022-04-25 19:26:47 +0300

    25.04.22

--- src/interviewQuestions8/Q04_MapTask.java
+++ src/interviewQuestions8/Q04_MapTask.java
@@ -1,5 +1,7 @@
 package interviewQuestions8;
 
+import java.util.*;
+
 public class Q04_MapTask {
     // PART 1
     // create a map with groupName as key and groupMembers as value
@@ -17,4 +19,38 @@ public class Q04_MapTask {
 
     // BÖLÜM 2
     // her grubun kaç üyesi olduğunu bul
+
+    public static void main(String[] args) {
+        HashMap<String, ArrayList<String>> grupIsimVeUyeler = new HashMap<>();
+
+        ArrayList<String> devTeam =new ArrayList<>(Arrays.asList("Abdullah", "Kutay", "Enes Faruk", "Yasin"));
+        ArrayList<String> qaTeam =new ArrayList<>(Arrays.asList("Kadir", "Seher", "Gokhan", "Bahattin"));
+
+        grupIsimVeUyeler.put("Development Team", devTeam);
+        grupIsimVeUyeler.put("QA Team", qaTeam);
+
+        System.out.println("grupIsimVeUyeler = " + grupIsimVeUyeler);
+        //grupIsimVeUyeler = {QA Team=[Kadir, Seher, Gokhan, Bahattin], Development Team=[Abdullah, Kutay, Enes Faruk, Yasin]}
+
+        //Bolum2
+        //1.yol -> value lardan giderek
+
+        Collection<ArrayList<String>> values = grupIsimVeUyeler.values();
+        for (ArrayList<String> kisi:values
+             ) {
+            System.out.println(kisi.size()); // ikisi icin de 4
+        }
+
+        //2.yol -> key lerden giderek
+
+        Set<String> keys = grupIsimVeUyeler.keySet(); //keySet() key leri set olarak dondurur
+        for (String key :keys
+             ) {
+            ArrayList<String> grupList =grupIsimVeUyeler.get(key);
+            System.out.println("gruptaki uye sayisi :" + key + " " + grupList.size());
+            //gruptaki uye sayisi :QA Team 4
+            //gruptaki uye sayisi :Development Team 4
+        }
+    }
+
 }
